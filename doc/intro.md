a39a986e196374fb852493a8c92ba0e1f0058b75
Max Edwards <maxedwar@thoughtworks.com>
2014-12-15 20:59:11 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to koomus.algorithms.binary
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/koomus/algorithms/binary_test.clj
@@ -0,0 +1,7 @@
+(ns koomus.algorithms.binary-test
+  (:require [clojure.test :refer :all]
+            [koomus.algorithms.binary :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
